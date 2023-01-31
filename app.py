49e9e52c334f03208739a7bb7db8e46bbb6ca3c0
Mahad Osman <mahado14@gmail.com>
2023-01-31 11:20:28 -0500

    Project base setup

---
+++ app.py
@@ -0,0 +1,19 @@
+"""
+Author: Mahad Osman
+Date: Jan 31 2021
+Assignment: Flask app for Cupcakes
+"""
+import re
+from flask import Flask, redirect, request, jsonify, render_template
+from models import db, connect_db, 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "secret-1-2-3"
+
+connect_db(app)
+@app.rout('/')
+def index():
+    return render_template()
